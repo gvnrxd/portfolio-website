0d29b8647d4b0b3d4ba0724d4c8aeb53b1160f40
gvnrds Windows Desktop <gvnrx@users.noreply.github.com>
2025-07-16 09:29:31 -0400

    Added first bit of javascript interactivity

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementsByClassName("project-button")[0].onmouseover = function(){
+    this.style.backgroundColor = "#58a6ff";
+};
+document.getElementsByClassName("project-button")[0].onmouseout = function(){
+    this.style.backgroundColor = "";
+};
